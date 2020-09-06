0afa9df663c41a99de065b9e68ed1b32fbaa0484
Gowtham2003 <gowtham13082003@gmail.com>
2020-09-06 13:17:17 +0530

    Handling Exceptions and Other Fixes

--- ign.py
+++ ign.py
@@ -4,19 +4,37 @@ from bs4 import BeautifulSoup as bs
 def getData():
     newsDict = {"data":[]}
 
-    url = "https://in.ign.com/?page=4&ist=broll"
-
-    r = requests.get(url)
+    url = "https://in.ign.com/?page=1&ist=broll"
+    try:
+        r = requests.get(url)
+    except Exception as e:
+        print("There's Somekind of Problem")
+        exit(-1)
 
     soup = bs(r.content,"lxml")
-
-    articles = soup.findAll("article",{"class":"article NEWS"})
+    try:
+        articles = soup.findAll("article",{"class":"article NEWS"})
+    except Exception as e:
+        print(e)
+        exit(-1)
 
     for article in articles:
-        title = article.img.get("alt")
-        link = article.a.get("href")
-        content = article.p.text
-        img = article.img.get("srcset").replace("2x","").replace(" ","").split(",")
+        try:
+            title = article.img.get("alt")
+        except:
+            title = ""
+        try:
+            link = article.a.get("href")
+        except:
+            link = ""
+        try:
+            content = article.p.text
+        except:
+            content = ""
+        try:
+            img = article.img.get("srcset").replace("2x","").replace(" ","").split(",")
+        except:
+            img = ""
 
         newsObject = {
                 "title":title,
@@ -28,5 +46,3 @@ def getData():
 
     return newsDict
 
-
-print(getData())

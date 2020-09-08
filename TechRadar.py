7480ae0705e4ead441244381ea6df6cbde67355f
Gowtham2003 <gowtham13082003@gmail.com>
2020-09-08 17:14:24 +0530

    Error Handling and Applied Autopep8

--- TechRadar.py
+++ TechRadar.py
@@ -1,22 +1,50 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
 def getData():
     url = "https://www.techradar.com/in/news"
-    r = requests.get(url)
-    soup = bs(r.content,"lxml")
-    dataList = soup.findAll("div",{"data-page":1})
+    try:
+        r = requests.get(url)
+    except Exception as e:
+        print(e)
+        exit(-1)
+    soup = bs(r.content, "lxml")
+    dataList = soup.findAll("div", {"data-page": 1})
     news = []
     for data in dataList:
-        title = data.find("a").get("aria-label")
-        title = data.find("h3",class_="article-name").text
-        url = data.find("a").get("href")
-        content = data.find("p",class_="synopsis").text.replace("Updated","").replace("How to watch","")
-        img = data.find("div",class_="image-remove-reflow-container landscape").get("data-original")
-        newsData = {                                              "title": title,                                   "content": content,                 
-                "imageUrl": img,                    
-                "newsUrl": url                                    }
+        #        title = data.find("a").get("aria-label")
+        try:
+            title = data.find("h3", class_="article-name").text
+        except BaseException:
+            title = ""
+        try:
+            url = data.find("a").get("href")
+        except BaseException:
+            url = ""
+        try:
+            content = data.find(
+                "p",
+                class_="synopsis").text.replace(
+                "Updated",
+                "").replace(
+                "How to watch",
+                "")
+        except BaseException:
+            content = ""
+        try:
+            img = data.find(
+                "div", class_="image-remove-reflow-container landscape").get("data-original")
+        except BaseException:
+            img = ""
+
+        newsData = {
+                "title": title,
+                "content": content,
+                "imageUrl": img,
+                "newsUrl": url}
         news.append(newsData)
     return news
+
 
 print(getData())

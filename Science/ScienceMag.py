32c9154b37bddda3c5d6d2895914f1b64b90ade4
truroshan <45506201+truroshan@users.noreply.github.com>
2020-09-20 13:58:54 +0530

    Added ScienceMag

---
+++ Science/ScienceMag.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup as bs
+# from pprint import pprint
+
+def getData():
+    url = "https://www.sciencemag.org/news/latest-news"
+
+    try:
+        r = requests.get(url)
+    except Exception as e:
+        print(e)
+        exit(-1)
+    soup = bs(r.content, "lxml")
+    dataList = soup.find('ul',class_ = 'headline-list').findAll("li") 
+
+    BASE_URL = 'https://www.sciencemag.org'
+    news = []
+    for data in dataList:
+        try:
+            title = data.find('h2').string.strip()
+        except BaseException:
+            title = ""
+        try:
+            url = BASE_URL + data.find('a').get("href")
+        except BaseException:
+            url = ""
+        try:
+            content = data.find('div',class_ = 'media__deck').string.strip()
+        except BaseException:
+            content = ""
+        try:
+            img = data.find('img').get("src")[2:]
+        except BaseException:
+            img = ""
+
+        newsData = {
+                "title": title,
+                "content": content,
+                "imageUrl": img,
+                "newsUrl": url}
+        news.append(newsData)
+    return news
+# pprint(getData())
